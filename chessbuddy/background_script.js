47e513a14f01b9e9a2bef044b8f47ecef224b7b8
Phil Johnson <phytoporg@gmail.com>
2021-08-02 15:41:46 -0700

    chessbuddy, listen for url requests in background script

--- chessbuddy/background_script.js
+++ chessbuddy/background_script.js
@@ -1,1 +1,19 @@
 // Put all the javascript code here, that you want to execute in background.
+
+function onNextTacticRequest(req) {
+    debug_log("Getting new tactics puzzle: " + req.url);
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    onNextTacticRequest,
+    {urls: ["*://*.chess.com/callback/tactics/learning/next"]}
+);
+
+function onSubmitMovesRequest(req) {
+    debug_log("Submitting moves for tactics puzzle: " + req.url);
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    onSubmitMovesRequest,
+    {urls: ["*://*.chess.com/callback/tactics/themeTraining/submitMoves"]}
+);
